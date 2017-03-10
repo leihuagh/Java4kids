580201886d0eef7ac7fd7e13b32d6080ab845f82
Lei Hua <lhua@masslottery.com>
2017-03-10 08:43:31 -0500

    java4kids modified menu.json for formatting

--- assets/js/home.js
+++ assets/js/home.js
@@ -5,24 +5,22 @@ $.ajaxSetup ({
 $(function() { 
     $.getJSON(dataPath + pathToData, function(json) { 
         init_menu(json);
-        $("span[id^='menu_']").click(onMenuClick)
+        //$("span[id^='menu_']").click(onMenuClick)
         //$("#menu_1_1").click(onMenuClick);
         
     });
 });
 
 function init_menu(json) { 
-    for (i in json.menu) { 
-        var menuitem = new Array();
-        menuitem["menu_id"] = json.menu[i].menu_id;   
-        menuitem["submenu_id"] = json.menu[i].submenu_id;   
-        menuitem["title"] = json.menu[i].title; 
-        menuitem["datapath"] = json.menu[i].datapath; 
-        menuitem["url"] = json.menu[i].url;
-        if (menu[json.menu[i].menu_id]===null) {
-            menu[json.menu[i].menu_id] = new A
+    for (i in json.menu) {
+        menu["menu_id"] = json.menu[i].menu_id; 
+        var max = parseInt(json.menu[i].children); 
+        for (var j=0; j<=max; j++) {
+            var submenu = new Array();
+            submenu["submenu_id"] = json.menu[i].submenu[j].submenu_id; 
+            submenu["title"] = json.menu[i].submenu[j].title;
+            submenu["datapath"] = json.menu[i].submenu[j].datapath;
         }
-        menu[json.menu[i].menu_id][json.menu[i].submenu_id] = menuitem;
     }
 }
 
